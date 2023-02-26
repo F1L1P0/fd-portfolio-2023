a7be0c0aaec12e6a28f9bcae034d9ea7a57554e6
Filip Drábek <drabekfilip@seznam.cz>
2023-02-26 14:01:52 +0100

    adding text resposivity and some resizing

--- src/components/componentsData/projectsData.ts
+++ src/components/componentsData/projectsData.ts
@@ -4,22 +4,20 @@ export default [
     imgSrc: "https://picsum.photos/600/400",
     url: "https://www.semag.cz/",
     name: "Semag",
-    describtion:
-      "Na zmíněném projektu jsem spolupracoval s firmou Simplo jako stážista během mého studia.",
+    describtion: "",
   },
   {
     id: 1,
     imgSrc: "https://picsum.photos/600/400",
-    url: "https://www.vscr.cz/",
+    url: "https://www.ostraticky.com/",
     name: "VSCR",
-    describtion:
-      "Na zmíněném projektu jsem spolupracoval s firmou Simplo jako stážista během mého studia.",
+    describtion: "",
   },
   {
     id: 2,
     imgSrc: "https://picsum.photos/600/400",
     url: "https://www.ostraticky.de/",
     name: "Ostraticky",
-    describtion: "Webová stránka vytvořená pomocí HTML, CSS, JS a Bootstrapu",
+    describtion: "",
   },
 ];
