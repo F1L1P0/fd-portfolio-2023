75dd21ab24279a54b1dc4eb2469142ec72792a19
Filip Drábek <drabekfilip@seznam.cz>
2023-03-09 12:07:43 +0100

    changes : changing texts and adding images + responsivnes to cards and contacts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,21 +7,27 @@ export default function Hero() {
       <div className="fd-hero__content">
         <ul>
           <li className="fd-hero__content__li">
-            Chtěli by jste webové stránky?
+            Zdravím!!! Jmenuji se Filip Drábek a je mi 22 let a jsem junior
+            fron-end developer.
           </li>
           <li className="fd-hero__content__li">
-            Máte vymyšlený design, ale potřebujete někoho, kdo vám ho pomůže
-            zrealizovat?
+            Webovému vývoji jsem se začal věnovat na střední škole, a od té doby
+            se v něm zdokonaluji.
+          </li>
+          <li className="fd-hero__content__li">
+            Momentálně pracuji částečně jako freelancer, ale chtěl bych se
+            posunou dále a stát se plnohodnodným vývojářem.
+          </li>
+          <li className="fd-hero__content__li">
+            Jsem otevřený jakékoliv nabídce práce / práci na projektu, který
+            zahrňuje front-end vývoj.
+          </li>
+          <li className="fd-hero__content__li">
+            Nejčastěji pracuji s HTML, SCSS, JS a poslední dobou se snažím
+            zlepšit své znalosti Reactu.
           </li>
         </ul>
-        <p>
-          Zdravím! Jmenuji se Filip Drábek a jsem vývojář webových stránek -
-          konkrétně front-endu.
-        </p>
-        <p>
-          O tohle téma se zajímám již od svých 17 let a stále se zdokonaluji.
-        </p>
-        <p>Zde se můžeš podívat co vše umím!</p>
+
         <div className="btn-center">
           <a href="#contact" className="btn">
             <span>Kontakt zde!</span>
--- src/components/componentsData/projectsData.ts
+++ src/components/componentsData/projectsData.ts
@@ -1,14 +1,20 @@
+import img1 from "../../images/ostraticky.png";
+import img2 from "../../images/semag.png";
+import img3 from "../../images/signin.png";
+import img4 from "../../images/oldportfolio.png";
+import img5 from "../../images/sidenav.png";
+
 export default [
   {
     id: 0,
-    imgSrc: "https://picsum.photos/600/400",
+    imgSrc: img1,
     url: "https://www.ostraticky.de/",
     name: "Ostraticky",
     describtion: "Website created by me on with HTML SCSS JS and Bootstrap",
   },
   {
     id: 1,
-    imgSrc: "https://picsum.photos/600/400",
+    imgSrc: img2,
     url: "https://www.semag.cz/",
     name: "Semag",
     describtion:
@@ -16,21 +22,21 @@ export default [
   },
   {
     id: 2,
-    imgSrc: "https://picsum.photos/600/400",
+    imgSrc: img3,
     url: "https://f1l1p0-sign-up-page.netlify.app/",
     name: "Simple sign-up page",
     describtion: "Very simple sign-up page",
   },
   {
     id: 3,
-    imgSrc: "https://picsum.photos/600/400",
-    url: "https://f1l1p0-sidebar.netlify.app/",
-    name: "Portfolio",
-    describtion: "Portfolio created",
+    imgSrc: img4,
+    url: "https://old-portfolio-template.netlify.app/",
+    name: "Portfolio template",
+    describtion: "Older portfolio template",
   },
   {
     id: 4,
-    imgSrc: "https://picsum.photos/600/400",
+    imgSrc: img5,
     url: "https://f1l1p0-sidebar.netlify.app/",
     name: "Simple modern side bar navigation",
     describtion: "Modern side bar navigation",
